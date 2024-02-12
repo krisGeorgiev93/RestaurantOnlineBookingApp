72323e871f177aa2f635d34053ed23d4f180da82
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-02-12 19:32:45 +0000

    created add restaurant form model

---
+++ RestaurantOnlineBookingApp/RestaurantOnlineBookingApp.Web.ViewModels/Category/SelectCategoryFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOnlineBookingApp.Web.ViewModels.Category
+{
+    public class SelectCategoryFormModel
+    {
+        //receive this info from DB and initialize it in the form
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+
+    }
+}
---
+++ RestaurantOnlineBookingApp/RestaurantOnlineBookingApp.Web.ViewModels/Restaurant/RestaurantFormModel.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace RestaurantOnlineBookingApp.Web.ViewModels.Restaurant
+{
+    using RestaurantOnlineBookingApp.Web.ViewModels.Category;
+    using System.ComponentModel.DataAnnotations;
+
+    using static RestaurantOnlineBookingApp.Common.ValidationConstants.Restaurant;
+    public class RestaurantFormModel
+    {
+        public RestaurantFormModel()
+        {
+            this.Categories = new HashSet<SelectCategoryFormModel>();
+        }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [MinLength(NameMinLength)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MaxLength(AddressMaxLength)]
+        [MinLength(AddressMinLength)]
+        public string Address { get; set; } = null!;
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        [MinLength(DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ImageUrlMaxLength)]
+        [Display(Name = "Image link")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(1, 5)]
+        public double Rating { get; set; }
+
+        [Required]
+        [Range(1, 300)]
+        public int Capacity { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectCategoryFormModel> Categories { get; set; }
+
+    }
+}
