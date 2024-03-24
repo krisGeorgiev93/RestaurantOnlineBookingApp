0a82a121965c71f1841dcfd1c8e43d33597d830a
Kristiyan Georgiev <krisgeorgiev027@gmail.com>
2024-03-24 19:35:04 +0000

    seed events class

---
+++ RestaurantOnlineBookingApp/RestaurantOnlineBookingApp.Data/Configurations/EventEntityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantOnlineBookingApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RestaurantOnlineBookingApp.Common.ValidationConstants;
+using Event = RestaurantOnlineBookingApp.Data.Models.Event;
+
+namespace RestaurantOnlineBookingApp.Data.Configurations
+{
+    public class EventEntityConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.HasData(this.UploadEvents());
+                        
+        }
+
+        private Event[] UploadEvents()
+        {
+            ICollection<Event> events = new HashSet<Event>();
+            Event @event;
+
+            @event = new Event()
+            {
+                Title = "Special Event 1",
+                Date = DateTime.Now.Date.AddDays(5),
+                Time = new TimeSpan(18, 0, 0),
+                Description = "Description for Special Event 1",
+                Price = 50.00m,
+                ImageUrl = "https://www.eventbookings.com/wp-content/uploads/2018/03/event-ideas-for-party-eventbookings.jpg",
+                RestaurantId = Guid.Parse("E681A1DD-B85F-4860-8EB9-CF517E5C4FE8")
+            };
+            events.Add(@event);
+
+            return events.ToArray();           
+        }
+    }
+}
--- RestaurantOnlineBookingApp/RestaurantOnlineBookingApp.Data/Configurations/RestaurantEntityConfiguration.cs
+++ RestaurantOnlineBookingApp/RestaurantOnlineBookingApp.Data/Configurations/RestaurantEntityConfiguration.cs
@@ -13,7 +13,7 @@ namespace RestaurantOnlineBookingApp.Data.Configurations
         }
         public void Configure(EntityTypeBuilder<Restaurant> builder)
         {
-          builder.HasData(this.UploadRestaurants());
+            builder.HasData(this.UploadRestaurants());
 
             builder.Property(r => r.IsActive)
                 .HasDefaultValue(true);
@@ -36,8 +36,25 @@ namespace RestaurantOnlineBookingApp.Data.Configurations
                .HasForeignKey(r => r.CityId)
                .OnDelete(DeleteBehavior.Restrict);
 
+            builder
+             .HasMany(r => r.Meals)
+             .WithOne(m => m.Restaurant)
+             .HasForeignKey(m => m.RestaurantId)
+             .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(r => r.Bookings)  
+            .WithOne(b => b.Restaurant)
+            .HasForeignKey(b => b.RestaurantId)
+             .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(r => r.Reviews)
+            .WithOne(b => b.Restaurant)
+            .HasForeignKey(b => b.RestaurantId)
+             .OnDelete(DeleteBehavior.Restrict);
+
+
             // Seed capacities after seeding restaurants
-            SeedCapacities();         
+            SeedCapacities();
 
         }
         private void SeedCapacities()
@@ -78,7 +95,7 @@ namespace RestaurantOnlineBookingApp.Data.Configurations
                 CategoryId = 1,
                 OwnerId = Guid.Parse("C4F8569C-1CDA-4B0B-94E4-16B44A4631CF")
             };
-            restaurants.Add(restaurant);           
+            restaurants.Add(restaurant);
 
             //restaurant = new Restaurant()
             //{
@@ -127,6 +144,6 @@ namespace RestaurantOnlineBookingApp.Data.Configurations
 
             return restaurants.ToArray();
         }
-       
+
     }
 }
